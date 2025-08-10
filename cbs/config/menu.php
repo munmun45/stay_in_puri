6c3f3f88b4592b80182cffeed7b27fc75a033066
munmun45 <manaranamunmun@gmail.com>
2025-08-10 22:57:34 +0530

    xcv

--- cbs/config/menu.php
+++ cbs/config/menu.php
@@ -13,14 +13,14 @@
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="amenities">
-                <i class="bi bi-calendar-event"></i>
+                <i class="bi bi-card-checklist"></i>
                 <span>Amenities</span>
             </a>
         </li>
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="hotel-listing">
-                <i class="bi bi-calendar-event"></i>
+                <i class="bi bi-building"></i>
                 <span>Hotel Listing</span>
             </a>
         </li>
@@ -29,7 +29,7 @@
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="room-listing">
-                <i class="bi bi-calendar-event"></i>
+                <i class="bi bi-door-open"></i>
                 <span>Room Listing</span>
             </a>
         </li>
@@ -37,7 +37,7 @@
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="price-maping">
-                <i class="bi bi-calendar-event"></i>
+                <i class="bi bi-tag-fill"></i>
                 <span>Price Mapping</span>
             </a>
         </li>
