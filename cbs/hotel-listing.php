e8fa9f4236d2e01819103386dc9a7fa374666d27
munmun45 <manaranamunmun@gmail.com>
2025-08-10 20:13:01 +0530

    dfg

---
+++ cbs/hotel-listing.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <?= require("./config/meta.php") ?>
+
+</head>
+
+<body>
+
+  <?= require("./config/header.php") ?>
+  <?= require("./config/menu.php") ?>
+
+
+
+
+
+
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Hotel Listing</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item active">Hotel Listing</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+      
+
+
+
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+
+
+
+
+
+
+
+  <?= require("./config/footer.php") ?>
+
+
+
+
+</body>
+
+</html>
