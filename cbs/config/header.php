046e97a01ab1225da2c1ca9e4e354fd228e826f7
munmun45 <manaranamunmun@gmail.com>
2025-08-11 13:08:15 +0530

    dfg

--- cbs/config/header.php
+++ cbs/config/header.php
@@ -2,8 +2,8 @@
 
     <div class="d-flex align-items-center justify-content-between">
         <a href="index" class="logo d-flex align-items-center">
-            <img src="assets/img/logo.png" alt="">
-            <span class="d-none d-lg-block">Stay In Puri</span>
+            <img src="../assets/img/stay-in-puri.png" alt="">
+            <span class="d-none d-lg-block">CBS Panel</span>
         </a>
         <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
