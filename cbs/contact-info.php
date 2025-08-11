c3fcc14b8a55852504e12c3ffcb4dafae73497c0
munmun45 <manaranamunmun@gmail.com>
2025-08-11 09:46:28 +0530

    fdg

---
+++ cbs/contact-info.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <?= require("./config/meta.php") ?>
+
+</head>
+
+<body>
+
+  <?= require("./config/header.php") ?>
+  <?= require("./config/menu.php") ?>
+
+
+
+
+
+
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Contact Info</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item active">Contact Info</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+      
+    
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+
+
+
+
+
+
+
+  <?= require("./config/footer.php") ?>
+
+
+
+
+</body>
+
+</html>
